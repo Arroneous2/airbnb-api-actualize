6965a6865a01b0a7d3b335dff6e8d0ad3f2c7372
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-11 18:03:17 -0500

    add seeds for rental

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Rental.create(user_id: 1, address: "10th st", city: "Chicago", state: "Il", price: 200, description: "this lovely 100sq ft shed...", home_type: "shed", rental_type: "room", total_occupancy: 1, total_bed_rentals: 0, total_bathrooms: 0)
+
+Rental.create(user_id: 2, address: "9th st", city: "Dubuque", state: "Ia", price: 100, description: "A retrofitted Barn", home_type: "barn", rental_type: "house", total_occupancy: 15, total_bed_rentals: 2, total_bathrooms: 1)
+
+Rental.create(user_id: 3, address: "8th st", city: "Houston", state: "Tx", price: 300, description: "A glamourous apartment", home_type: "apartment", rental_type: "house", total_occupancy: 3, total_bed_rentals: 1, total_bathrooms: 2)
+
+Rental.create(user_id: 3, address: "7th st", city: "Houston", state: "Tx", price: 250, description: "an open floor concept bungalo", home_type: "bungalo", rental_type: "house", total_occupancy: 4, total_bed_rentals: 3, total_bathrooms: 2)
