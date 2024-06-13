9612545679772f09395fe620c61f21a5bf720876
laaronchang <aacidus@gmail.com>
2024-06-12 22:21:29 -0700

    added user base files and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.new(name: "Alicia", email: "alicia@stone.com", password: "password", image: "https://imageio.forbes.com/blogs-images/jenglantz/files/2018/09/Screen-Shot-2018-09-10-at-3.34.49-PM.png")
+user.save
+
+user = User.new(name: "Albert", email: "albert@einstein.com", password: "password", image: "https://nationaltoday.com/wp-content/uploads/2022/04/Albert-Einstein-Birthday-1200x834.jpg.webp")
+user.save
+
+user = User.new(name: "Bruce", email: "bruce@wayne.com", password: "password", image: "https://static.wikia.nocookie.net/batman/images/8/8a/Adam_West.jpg")
+user.save
+
+user = User.new(name: "Edgar", email: "edgar@poe.com", password: "password", image: "https://hadikarimi.com/wp-content/uploads/2020/12/Edgar-1.jpg")
+user.save
