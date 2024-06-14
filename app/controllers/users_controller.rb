846e8f4283aec733842a2445cdf0239a712f5440
laaronchang <aacidus@gmail.com>
2024-06-14 11:22:43 -0700

    updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-    render template:"users/index"
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(
+      name: params[:name] || @user.name,
+      email: params[:email] || @user.email,
+      image: params[:image_url] || @user.image,
+    )
+    render :show
   end
 end
