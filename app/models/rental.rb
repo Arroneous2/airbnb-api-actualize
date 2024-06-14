06a268aa8a59c9a878caad4ec18d67aaf9d26226
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-14 15:43:10 -0500

    start linking tables

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,5 @@
 class Rental < ApplicationRecord
   has_many :rental_images
+  has_many :reviews
   belongs_to :user
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :rental
   validates :rating, :inclusion => 1..5 
 end
