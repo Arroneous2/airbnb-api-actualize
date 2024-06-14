0c72a34a556b80c3ff888e9c2472ec9dc97e3da4
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-14 15:50:02 -0500

    finish linking tables

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :rental
+  belongs_to :reservation
   validates :rating, :inclusion => 1..5 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :rentals
   has_many :reviews
+  has_many :reservations
 
-has_secure_password
-validates :email, presence: true, uniqueness: true
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 end
